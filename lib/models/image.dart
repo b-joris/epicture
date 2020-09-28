963888bd2c0983fa8f7fde77d78b40239b572048
Elias El Fallah <elias.el-fallah@epitech.eu>
2020-09-28 15:27:21 +0200

    add(model): adding Imgur Image model

---
+++ lib/models/image.dart
@@ -0,0 +1,25 @@
+class Image {
+  final String id;
+  final String title;
+  final String description;
+  final String type;
+  final String link;
+  bool isFavorite;
+
+  Image({
+    this.id,
+    this.title,
+    this.description,
+    this.type,
+    this.link,
+    this.isFavorite,
+  });
+
+  Image.fromJson(Map<String, dynamic> data)
+      : this.id = data['id'],
+        this.title = data['title'],
+        this.description = data['description'],
+        this.type = data['type'],
+        this.link = data['link'],
+        this.isFavorite = data['favorite'];
+}
