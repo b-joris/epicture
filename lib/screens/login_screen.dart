0a6d1ddab86cfbe3803ee93a4b1e1d4b0b7d4e3b
Elias El Fallah <elias.el-fallah@epitech.eu>
2020-09-29 20:23:18 +0200

    add(screen): adding Login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,41 @@
+import 'package:epicture/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+import '../constants.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final webview = FlutterWebviewPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    webview.onStateChanged.listen((event) {
+      var uri = Uri.parse(event.url.replaceFirst('#', '?'));
+      if (uri.query.contains('access_token')) {
+        print(uri.queryParameters['access_token']);
+        sharedPreferences.setString(
+            'access_token', uri.queryParameters['access_token']);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    webview.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url:
+          'https://api.imgur.com/oauth2/authorize?client_id=$clientID&response_type=token',
+    );
+  }
+}
