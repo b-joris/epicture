627b308c451c0ed56a6f6b87e329c3c6d2ea04cd
Joris Bertaigne <git@jorisb.dev>
2020-09-29 19:50:14 +0200

    add(bloc): adding Gallery Bloc

---
+++ lib/blocs/gallery_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:epicture/models/post.dart';
+import 'package:epicture/networking/response.dart';
+import 'package:epicture/repositories/gallery_repository.dart';
+
+class GalleryBloc {
+  GalleryRepository _repository;
+  StreamController _controller;
+
+  GalleryBloc() {
+    _repository = GalleryRepository();
+    _controller = StreamController<Response<List<Post>>>();
+    fetchGallery();
+  }
+
+  dispose() {
+    _controller?.close();
+  }
+
+  StreamSink<Response<List<Post>>> get gallerySink => _controller.sink;
+  Stream<Response<List<Post>>> get galleryStream => _controller.stream;
+
+  fetchGallery() async {
+    gallerySink.add(Response.loading('Getting Gallery Posts'));
+
+    try {
+      List<Post> posts = await _repository.fetchGalleryData();
+      gallerySink.add(Response.completed(posts));
+    } catch (exception) {
+      gallerySink.add(Response.error(exception.toString()));
+      print(exception);
+    }
+  }
+}
