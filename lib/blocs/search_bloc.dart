d54106175eba2afa5e1436f5a314ebc1675fb5f9
Elias El Fallah <elias.el-fallah@epitech.eu>
2020-09-29 21:50:24 +0200

    add(bloc): adding Search bloc

---
+++ lib/blocs/search_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:epicture/models/post.dart';
+import 'package:epicture/networking/response.dart';
+import 'package:epicture/repositories/search_repository.dart';
+
+class SearchBloc {
+  SearchRepository _repository;
+  StreamController _controller;
+
+  SearchBloc() {
+    _repository = SearchRepository();
+    _controller = StreamController<Response<List<Post>>>();
+  }
+
+  dispose() {
+    _controller?.close();
+  }
+
+  StreamSink<Response<List<Post>>> get searchSink => _controller.sink;
+  Stream<Response<List<Post>>> get searchStream => _controller.stream;
+
+  fetchSearch(String query) async {
+    searchSink.add(Response.loading('Getting Search Posts'));
+
+    try {
+      List<Post> posts = await _repository.fetchSearchData(query);
+      searchSink.add(Response.completed(posts));
+    } catch (exception) {
+      searchSink.add(Response.error(exception.toString()));
+      print(exception);
+    }
+  }
+}
