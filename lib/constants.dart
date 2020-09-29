f7d31e05efa15f6f83f041d3af05efa06065cab2
Joris Bertaigne <git@jorisb.dev>
2020-09-29 23:48:48 +0200

    feat(constant): adding new constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,8 @@
+import 'package:flutter/material.dart';
+
 const clientID = String.fromEnvironment('CLIENT_ID');
+
+const primaryColor = Colors.green;
+const accentColor = Colors.greenAccent;
+
 const defaultPadding = 10.0;
