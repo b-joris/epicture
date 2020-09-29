5abbb9bb9e06450953cb9d89342788134b3da5a4
Elias El Fallah <elias.el-fallah@epitech.eu>
2020-09-29 21:51:28 +0200

    add(screen): adding Search screen

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,65 @@
+import 'package:epicture/blocs/search_bloc.dart';
+import 'package:epicture/constants.dart';
+import 'package:epicture/models/post.dart';
+import 'package:epicture/networking/response.dart';
+import 'package:epicture/widgets/post_card.dart';
+import 'package:epicture/widgets/search_bar.dart';
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final _bloc = SearchBloc();
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: SearchBar(onSubmitted: _bloc.fetchSearch),
+          ),
+          StreamBuilder(
+            stream: _bloc.searchStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                switch (snapshot.data.status) {
+                  case Status.LOADING:
+                    return Expanded(
+                        child: Center(child: CircularProgressIndicator()));
+                  case Status.COMPLETED:
+                    final List<Post> posts = snapshot.data.data;
+                    return Expanded(
+                      child: ListView.builder(
+                        itemCount: posts.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.all(defaultPadding),
+                            child: PostCard(posts[index]),
+                          );
+                        },
+                      ),
+                    );
+                }
+              }
+              return Container();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
