ab1fc894cf52dc4456de4790aa5494470ac2fd1d
Elias El Fallah <elias.el-fallah@epitech.eu>
2020-10-20 22:43:39 +0200

    refactor(navigation): creating navigation bar file

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:epicture/screens/details_screen.dart';
 import 'package:epicture/screens/gallery_screen.dart';
 import 'package:epicture/screens/login_screen.dart';
 import 'package:epicture/screens/search_screen.dart';
+import 'package:epicture/widgets/navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -16,51 +17,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   sharedPreferences = await SharedPreferences.getInstance();
   runApp(Epicture());
-}
-
-class NavigationBar extends StatefulWidget {
-  @override
-  _NavigationBarState createState() => _NavigationBarState();
-}
-
-class _NavigationBarState extends State<NavigationBar> {
-  int _currentIndex = 0;
-
-  final screens = [
-    GalleryScreen(),
-    SearchScreen(),
-    AccountScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.collections),
-            label: 'Gallery',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Account',
-          ),
-        ],
-      ),
-      body: screens[_currentIndex],
-    );
-  }
 }
 
 class Epicture extends StatefulWidget {
---
+++ lib/widgets/navigation_bar.dart
@@ -0,0 +1,49 @@
+import 'package:epicture/screens/account_screen.dart';
+import 'package:epicture/screens/gallery_screen.dart';
+import 'package:epicture/screens/search_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatefulWidget {
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  int _currentIndex = 0;
+
+  final screens = [
+    GalleryScreen(),
+    SearchScreen(),
+    AccountScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.collections),
+            label: 'Gallery',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+      body: screens[_currentIndex],
+    );
+  }
+}
